45db783dfacbead867cfd6417f046ba207609ff9
Cathy Chen <cathychenn@gmail.com>
2019-03-23 17:11:20 -0700

    store shit

--- server/messenger/handler.py
+++ server/messenger/handler.py
@@ -1,5 +1,17 @@
+from models import ChatState, ClientData
+from store import DB as db
+
 def handleMessage(sender_psid, received_message):
   print(received_message);
 
-def callSendAPI(sender_psid, response):
-  pass
+  # SAVE reponse in CLIENT DATA
+
+  # Fetch most recent chat state row for this sender_psid
+  # Send next message depending on the previous prompt key
+
+def callSendAPI(sender_psid, prompt_key):
+  new_chat_state = ChatState(sender_psid, prompt_key)
+  db.session.add(new_chat_state)
+  db.session.commit()
+
+  # SEND TO MESSENGER
