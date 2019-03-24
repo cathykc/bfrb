6b95bcd9a3524a01c189aa14f55bc08cbcc01ee6
Cathy Chen <cathychenn@gmail.com>
2019-03-23 18:13:00 -0700

    alex comment

--- client/src/config/Config.tsx
+++ client/src/config/Config.tsx
@@ -1,5 +1,25 @@
 import * as React from 'react';
 
+// Hello Alex
+// Essentially we want a component that's mounted on this page to edit something of this schema:
+
+// [
+//   {
+//     prompt_key: 'site', // str, uniq_id
+//     prompt_text: 'Where are you picking?',  // str
+//     response_type: 'choice' | 'text' // is the response multi-choice or free text
+//     response_options: ['face', 'lip', 'hair'] // exists if response_type == 'choice'
+//   },
+//   {
+//     prompt_key: 'another_example',
+//     prompt_text: 'Where are you?',
+//     prompt_type: 'text'
+//   },
+// ]
+
+// props
+// 1. json of old config
+// 2. handler to callback with new config json
 export default class Config extends React.Component {
   public render(): JSX.Element {
     return(
